ccdf2ee601e20a7c1e8343d68ee598c19cbea760
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2020-12-06 22:59:30 +0100

    menu link onclick function  and window onclick function added

--- portfolio.js
+++ portfolio.js
@@ -1,13 +1,25 @@
 const jump = document.querySelector('.jump');
 const header = document.querySelector('header');
+const menuLink = document.querySelector('.menu-link');
+const menuList = document.querySelector('.menu-list');
+
+window.onload = function() {
+    document.body.className += " loaded";
+}
 
 window.onscroll = function() {
     displayLink();
     displayShadow();
 };
 
-window.onload = function() {
-    document.body.className += " loaded";
+menuLink.onclick = function() {
+    displayDropdown();
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.menu-link') & !event.target.matches('.menu') & (menuList.classList.contains('show'))) {
+            menuList.classList.remove('show');
+    }
 }
 
 function displayLink() {
@@ -25,3 +37,7 @@ function displayShadow() {
         header.classList.remove('header-scrolled');
     }
 }
+
+function displayDropdown() {
+    menuList.classList.toggle("show");
+  }
