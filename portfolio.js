f1c99b872b3561cdd338e43938e98884706d829b
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2021-01-14 12:20:49 +0100

    Intersection observer added starting slide-up animation at thr right moment

--- portfolio.js
+++ portfolio.js
@@ -56,3 +56,27 @@ function copyEmail() {
         alert("Email not copied");
       });
 }
+
+if ('IntersectionObserver' in window) {
+
+    let callback = (entries, observer) => {
+        entries.forEach(entry => {
+            entry.target.classList.add('opacity-off');
+            if(entry.isIntersecting) {
+                entry.target.classList.add('slide-start');
+                observer.unobserve(entry.target);
+            }
+        });
+    };
+
+    let options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.2
+    }
+
+    let observer = new IntersectionObserver(callback, options);
+
+    let targets = document.querySelectorAll('.slide-up');
+    targets.forEach(target => observer.observe(target));
+}
