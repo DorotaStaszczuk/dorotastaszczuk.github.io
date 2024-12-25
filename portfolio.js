7fa7617ec3fff1af92f2202456eff338ba095dab
Dorota <dorota.staszczuk@gmail.com>
2024-12-25 21:18:45 +0100

    fix: code formatting corrected

--- portfolio.js
+++ portfolio.js
@@ -6,104 +6,119 @@ const copyButton = document.querySelector('.copy-button');
 const email = document.querySelector('.email');
 let emailText = email.textContent;
 
-window.onload = function() {
-    document.body.className += " loaded";
-}
+window.onload = function () {
+  document.body.className += ' loaded';
+};
 
-window.onscroll = function() {
-    displayLink();
-    displayShadow();
-}
+window.onscroll = function () {
+  displayLink();
+  displayShadow();
+};
 
-menuLink.onclick = function() {
-    displayDropdown();
-}
+menuLink.onclick = function () {
+  displayDropdown();
+};
 
-window.onclick = function(event) {
-    if (!event.target.matches('.menu-link') & !event.target.matches('.menu') & (menuList.classList.contains('show'))) {
-            menuList.classList.remove('show');
-    }
-}
+window.onclick = function (event) {
+  if (
+    !event.target.matches('.menu-link') &
+    !event.target.matches('.menu') &
+    menuList.classList.contains('show')
+  ) {
+    menuList.classList.remove('show');
+  }
+};
 
-copyButton.onclick = function() {
-    copyEmail();
-}
+copyButton.onclick = function () {
+  copyEmail();
+};
 
 function displayLink() {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        jump.classList.remove('none');
-    } else {
-        jump.classList.add('none');
-    }
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    jump.classList.remove('none');
+  } else {
+    jump.classList.add('none');
+  }
 }
 
 function displayShadow() {
-    if (document.body.scrollTop > 2 || document.documentElement.scrollTop > 2) {
-        header.classList.add('header-scrolled');
-    } else {
-        header.classList.remove('header-scrolled');
-    }
+  if (document.body.scrollTop > 2 || document.documentElement.scrollTop > 2) {
+    header.classList.add('header-scrolled');
+  } else {
+    header.classList.remove('header-scrolled');
+  }
 }
 
 function displayDropdown() {
-    menuList.classList.toggle("show");
+  menuList.classList.toggle('show');
 }
 
 function copyEmail() {
-    navigator.clipboard.writeText(emailText).then(function() {
-        alert("Email copied successfully");
-      }, function() {
-        alert("Email not copied");
-      });
+  navigator.clipboard.writeText(emailText).then(
+    function () {
+      alert('Email copied successfully');
+    },
+    function () {
+      alert('Email not copied');
+    },
+  );
 }
 
 if ('IntersectionObserver' in window) {
+  let callback = (entries, observer) => {
+    entries.forEach((entry) => {
+      entry.target.classList.add('opacity-off');
+      if (entry.isIntersecting) {
+        entry.target.classList.add('slide-start');
+        observer.unobserve(entry.target);
+      }
+    });
+  };
 
-    let callback = (entries, observer) => {
-        entries.forEach(entry => {
-            entry.target.classList.add('opacity-off');
-            if(entry.isIntersecting) {
-                entry.target.classList.add('slide-start');
-                observer.unobserve(entry.target);
-            }
-        });
-    };
+  let options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.2,
+  };
 
-    let options = {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0.2
-    }
+  let observer = new IntersectionObserver(callback, options);
 
-    let observer = new IntersectionObserver(callback, options);
+  let targets = document.querySelectorAll('.slide-up');
+  targets.forEach((target) => observer.observe(target));
 
-    let targets = document.querySelectorAll('.slide-up');
-    targets.forEach(target => observer.observe(target));
+  let sectionCallback = (entries) => {
+    entries.forEach((entry) => {
+      const id = entry.target.getAttribute('id');
+      if (entry.intersectionRatio > 0.1) {
+        document
+          .querySelector(`nav li a[href="#${id}"]`)
+          .parentElement.classList.add('active');
+      } else {
+        document
+          .querySelector(`nav li a[href="#${id}"]`)
+          .parentElement.classList.remove('active');
+      }
+    });
+  };
 
+  let sectionOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1,
+  };
 
-    let sectionCallback = (entries) => {
-        entries.forEach(entry => {
-            const id = entry.target.getAttribute('id');
-        if (entry.intersectionRatio > 0.1) {
-            document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
-        } else {
-            document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
-        }
-        });
-    };
+  let sectionObserver = new IntersectionObserver(
+    sectionCallback,
+    sectionOptions,
+  );
 
-    let sectionOptions = {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0.1
-    }
-
-    let sectionObserver = new IntersectionObserver(sectionCallback, sectionOptions);
-
-    let sections = document.querySelectorAll('section[id]');
-    sections.forEach((section) => sectionObserver.observe(section));
+  let sections = document.querySelectorAll('section[id]');
+  sections.forEach((section) => sectionObserver.observe(section));
 }
 
-    const currentYear  = document.querySelector('.current-year');
-    const date = new Date();
-    currentYear.innerText = date.getFullYear();
+const currentYear = document.querySelector('.current-year');
+const date = new Date();
+currentYear.innerText = date.getFullYear();
